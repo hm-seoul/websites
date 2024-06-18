3d7eeb156a1c36bdbf006bc04a6242da1d99866a
Hyeseong Kim <hey@hyeseong.kim>
2024-06-19 00:35:51 +0900

    downcase

--- soundly-typed.fun/public/static/onerror.js
+++ soundly-typed.fun/public/static/onerror.js
@@ -1,11 +1,12 @@
 window.addEventListener('error', () => {
   switch (window.location.hostname) {
     case 'typed.fun':
-      document.querySelector('main').append(' Something wrong (See the inspector)');
+      document.querySelector('main').append(' something wrong (See the inspector)');
       console.error('You need to go https://soundly-typed.fun');
       break;
+
     case 'untyped.fun':
-      document.querySelector('main').append(' Something wrong (See the inspector)');
+      document.querySelector('main').append(' something wrong (See the inspector)');
       console.error('You need to go https://typed.fun');
       break;
   }
