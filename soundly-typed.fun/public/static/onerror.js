79ae1df5ac48c7742307735fcc3c4fe0426292bf
Hyeseong Kim <hey@hyeseong.kim>
2024-06-19 00:37:06 +0900

    comments

--- soundly-typed.fun/public/static/onerror.js
+++ soundly-typed.fun/public/static/onerror.js
@@ -1,7 +1,7 @@
 window.addEventListener('error', () => {
   switch (window.location.hostname) {
     case 'typed.fun':
-      document.querySelector('main').append(' something wrong (See the inspector)');
+      document.querySelector('main').append(' still something wrong (See the inspector)');
       console.error('You need to go https://soundly-typed.fun');
       break;
 
